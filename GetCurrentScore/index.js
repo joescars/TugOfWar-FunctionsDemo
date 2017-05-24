368b50f4fd3de6b27bc310ff699b7e50606fc410
Joe Raio <joe.raio@microsoft.com>
2017-05-24 22:29:15 +0200

    updates

--- GetCurrentScore/index.js
+++ GetCurrentScore/index.js
@@ -1,16 +1,24 @@
 module.exports = function (context, req) {
    
+    var Teams = [];
     var documents = context.bindings.documents;
     for (var i = 0; i < documents.length; i++) {
         var document = documents[i];
         context.log(document);
+        var t = new Team(document.TeamName,document.Score);
+        Teams.push(t);
     }     
 
     context.res = {
         status: 200,
-        body: documents
+        body:  JSON.stringify(Teams)
     };
 
     context.done();
 
 };
+
+function Team(teamNameIn, scoreIn) {
+    this.TeamName = teamNameIn;
+    this.Score = scoreIn;
+}
