bcfb36155fdaaec809791c970093fd4f5950869a
Joe Raio <joe.raio@microsoft.com>
2017-06-16 11:52:50 -0400

    Code Cleanup. Moved js into files

---
+++ Client/index.js
@@ -0,0 +1,115 @@
+$( document ).ready(function() {
+    // do nothing... 
+});
+
+var Team1Name = "Team1";
+var Team2Name = "Team2";                
+
+var Team1Score = 0;
+var Team2Score = 0;
+
+var siteDomain = "https://" + document.domain
+
+$("#btnLetsPlay").click(function(){
+    
+    // set team names
+    Team1Name = $("#nameTeam1").val();
+    Team2Name = $("#nameTeam2").val();
+
+    // display team names
+    $("#team1Display").text(Team1Name);
+    $("#team2Display").text(Team2Name);
+
+    // create the teams in the db
+    setupTeams();
+
+    // kick off the process
+    updateScore();
+
+}); 
+
+var setupTeams = function() {
+
+    var teamsData = 
+        {
+            "team1" : Team1Name,
+            "team2" : Team2Name
+        }
+
+    $.ajax({
+            url : siteDomain + "/createteams",
+            type: "POST",
+            data: JSON.stringify(teamsData),
+            contentType: "application/json",
+            dataType   : "json",
+            success    : function(data){
+                console.log("Teams Created");
+            }
+        });
+
+};
+
+var updateScore = function() {
+    
+    console.log("---------------")
+    var scoreResult;
+
+    var team1Data = {"team" : Team1Name};
+    var team2Data = {"team" : Team2Name};                
+
+    getScore(team1Data,1);
+    getScore(team2Data,2);
+
+    console.log("Team 1: " + Team1Score);
+    console.log("Team 2: " + Team2Score);
+                
+    // Update scores
+    refreshScore(Team1Score, Team2Score);
+
+    // move logo based on scores
+    moveLogo(Team1Score, Team2Score);
+
+    // fire off the same function shortly
+    rinseRepeat();                    
+
+};
+
+var getScore = function(teamData,teamNum) {
+
+    $.ajax({
+            url : siteDomain + "/getscores",
+            type: "POST",
+            data: JSON.stringify(teamData),
+            contentType: "application/json",
+            dataType   : "json",
+            success    : function(data){
+                var Result = JSON.parse(data);
+                if(teamNum == 1) {
+                    Team1Score = Result.Score;
+                } else {
+                    Team2Score = Result.Score;
+                }
+            }
+        });
+
+};
+
+var refreshScore = function(score1, score2) {
+    $("#team1").text(score1);
+    $("#team2").text(score2);
+};
+
+var moveLogo = function(score1, score2) {
+    // do the math
+    var toMove = parseInt(score1) - parseInt(score2);    
+    console.log("Moving: " + toMove);
+        
+    // move logo based on scores
+    $("#logo").css({
+        left: $("#logo").position().left - toMove + "px"
+    });                  
+};
+
+var rinseRepeat = function() {
+    setTimeout(updateScore, 1000);
+};
---
+++ Client/play.js
@@ -0,0 +1,68 @@
+$( document ).ready(function() {
+    
+    // get team settings
+    getSettings();
+
+});
+
+var Team1Name = "Loading... ";
+var Team2Name = "Loading... ";
+
+var siteDomain = "https://" + document.domain
+
+$("#team1btn").click(function(){
+    vote(Team1Name);
+}); 
+
+$("#team2btn").click(function(){
+    vote(Team2Name);
+});    
+
+var vote = function(teamId) {
+    $.ajax({
+            url : siteDomain + "/incrementpoint",
+            type: "POST",
+            data: JSON.stringify(
+                {team: teamId}
+            ),
+            contentType: "application/json; charset=utf-8",
+            dataType   : "json",
+            success    : function(){
+                console.log("Voted");
+            }
+        });                
+};      
+
+var getSettings = function() {
+
+    var currScore = 0;
+
+    function setScore(score) {
+        currScore = score;
+    }
+
+    $.ajax({
+            url : siteDomain + "/getsettings",
+            type: "GET",
+            contentType: "application/json",
+            dataType   : "json",
+            success    : function(data){
+                
+                var Result = JSON.parse(data);
+                Team1Name = Result.team1;
+                Team2Name = Result.team2;
+
+                // set button labels
+                setupLabels();        
+
+            }
+        });
+
+    return currScore;
+
+};            
+
+var setupLabels = function() {
+        $("#team1btn").text("Team: " + Team1Name);
+        $("#team2btn").text("Team: " + Team2Name);
+}
